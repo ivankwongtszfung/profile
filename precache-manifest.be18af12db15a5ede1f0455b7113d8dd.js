ceff232daaa2ae651613120c72e9346259925119
Ivan Kwong <ivankwongtszfung@gmail.com>
2020-05-01 22:32:01 +0800

    test commit for loading a webpage of a portfolio

---
+++ precache-manifest.be18af12db15a5ede1f0455b7113d8dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a24a5315614ec651ce487f6cedd291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb6f4432c03b6a0fede",
+    "url": "/static/css/main.1534166d.chunk.css"
+  },
+  {
+    "revision": "931ec45c5ca3262f6a55",
+    "url": "/static/js/2.d435d702.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/static/js/2.d435d702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb6f4432c03b6a0fede",
+    "url": "/static/js/main.d7f5bfce.chunk.js"
+  },
+  {
+    "revision": "d3216753ca9aab8acf68",
+    "url": "/static/js/runtime-main.55a6931c.js"
+  }
+]);
